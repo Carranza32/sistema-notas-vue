6b518f93b570f0161f82ae0a98d29551bccc9099
Julio Aguilar <julioa.m777@hotmail.com>
2022-04-15 12:34:17 -0600

    changes on error, login, grade, report, student, subjetct and teacher

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -26,44 +26,38 @@
   </v-card-title>-->
         <!-- title -->
         <v-card-text>
-          <p class="text-2xl font-weight-semibold text--primary mb-2">
-            Colegio Steve Jobs, te da la bienvenida.
-          </p>
-          <p class="mb-2">
-            Por favor, inicia sesion.
-          </p>
+          <p class="text-2xl font-weight-semibold text--primary mb-2">Colegio Steve Jobs, te da la bienvenida.</p>
+          <p class="mb-2">Por favor, inicia sesion.</p>
         </v-card-text>
 
         <!-- login form -->
         <v-card-text>
           <v-form>
-            <v-text-field v-model="email"
-                          outlined
-                          label="Usuario"
-                          placeholder="john@example.com"
-                          hide-details
-                          class="mb-3"></v-text-field>
+            <v-text-field
+              v-model="email"
+              outlined
+              label="Usuario"
+              placeholder="john@example.com"
+              hide-details
+              class="mb-3"
+            ></v-text-field>
 
-            <v-text-field v-model="password"
-                          outlined
-                          :type="isPasswordVisible ? 'text' : 'password'"
-                          label="Contraseña"
-                          placeholder="············"
-                          :append-icon="isPasswordVisible ? icons.mdiEyeOffOutline : icons.mdiEyeOutline"
-                          hide-details
-                          @click:append="isPasswordVisible = !isPasswordVisible"></v-text-field>
+            <v-text-field
+              v-model="password"
+              outlined
+              :type="isPasswordVisible ? 'text' : 'password'"
+              label="Contraseña"
+              placeholder="············"
+              :append-icon="isPasswordVisible ? icons.mdiEyeOffOutline : icons.mdiEyeOutline"
+              hide-details
+              @click:append="isPasswordVisible = !isPasswordVisible"
+            ></v-text-field>
 
             <div class="d-flex align-center justify-space-between flex-wrap">
-              <v-checkbox label="Recordar usuario"
-                          hide-details
-                          class="me-3 mt-1">
-              </v-checkbox>
+              <v-checkbox label="Recordar usuario" hide-details class="me-3 mt-1"> </v-checkbox>
 
               <!-- forgot link -->
-              <a href="javascript:void(0)"
-                 class="mt-1">
-                Olvidaste tu contraseña?
-              </a>
+              <a href="javascript:void(0)" class="mt-1"> Olvidaste tu contraseña? </a>
             </div>
             <!-- divider -->
             <v-card-text class="d-flex align-center mt-2">
@@ -72,22 +66,14 @@
               <v-divider></v-divider>
             </v-card-text>
 
-            <v-btn block
-                   color="primary"
-                   class="mt-6">
-              Iniciar sesion
-            </v-btn>
+            <v-btn block color="primary" class="mt-6" @click="login"> Iniciar sesion </v-btn>
           </v-form>
         </v-card-text>
 
         <!-- create new account  -->
         <v-card-text class="d-flex align-center justify-center flex-wrap mt-2">
-          <span class="me-2">
-            Eres nuevo en la plataforma?
-          </span>
-          <router-link :to="{name:'pages-register'}">
-            Solicita creacion de cuenta
-          </router-link>
+          <span class="me-2"> Eres nuevo en la plataforma? </span>
+          <router-link :to="{ name: 'pages-register' }"> Solicita creacion de cuenta </router-link>
         </v-card-text>
 
         <!-- divider -->
@@ -110,80 +96,95 @@
     </v-btn>
   </v-card-actions>-->
       </v-card>
-</div>
+    </div>
 
-  <!-- background triangle shape  -->
-        <img class="auth-mask-bg"
-             height="500"
-             :src="require(`@/assets/images/misc/mask-${$vuetify.theme.dark ? 'dark':'light'}.png`)">
+    <!-- background triangle shape  -->
+    <img
+      class="auth-mask-bg"
+      height="500"
+      :src="require(`@/assets/images/misc/mask-${$vuetify.theme.dark ? 'dark' : 'light'}.png`)"
+    />
 
-        <!-- tree -->
-        <!--
+    <!-- tree -->
+    <!--
   <v-img
     class="auth-tree"
     width="247"
     height="185"
     src="@/assets/images/misc/tree.png"
   ></v-img>-->
-        <!-- tree  -->
-        <!--
+    <!-- tree  -->
+    <!--
   <v-img
     class="auth-tree-3"
     width="377"
     height="289"
     src="@/assets/images/misc/tree-3.png"
   ></v-img>-->
-</div>
+  </div>
 </template>
 
 <script>
-  // eslint-disable-next-line object-curly-newline
-  import { mdiFacebook, mdiTwitter, mdiGithub, mdiGoogle, mdiEyeOutline, mdiEyeOffOutline } from '@mdi/js'
-  import { ref } from '@vue/composition-api'
+// eslint-disable-next-line object-curly-newline
+import { mdiFacebook, mdiTwitter, mdiGithub, mdiGoogle, mdiEyeOutline, mdiEyeOffOutline } from '@mdi/js'
+import { ref } from '@vue/composition-api'
+import axios from 'axios'
 
-  export default {
-    setup() {
-      const isPasswordVisible = ref(false)
-      const email = ref('')
-      const password = ref('')
-      const socialLink = [
-        {
-          icon: mdiFacebook,
-          color: '#4267b2',
-          colorInDark: '#4267b2',
-        },
-        {
-          icon: mdiTwitter,
-          color: '#1da1f2',
-          colorInDark: '#1da1f2',
-        },
-        {
-          icon: mdiGithub,
-          color: '#272727',
-          colorInDark: '#fff',
-        },
-        {
-          icon: mdiGoogle,
-          color: '#db4437',
-          colorInDark: '#db4437',
-        },
-      ]
+export default {
+  setup() {
+    const isPasswordVisible = ref(false)
+    const email = ref('')
+    const password = ref('')
+    const socialLink = [
+      {
+        icon: mdiFacebook,
+        color: '#4267b2',
+        colorInDark: '#4267b2',
+      },
+      {
+        icon: mdiTwitter,
+        color: '#1da1f2',
+        colorInDark: '#1da1f2',
+      },
+      {
+        icon: mdiGithub,
+        color: '#272727',
+        colorInDark: '#fff',
+      },
+      {
+        icon: mdiGoogle,
+        color: '#db4437',
+        colorInDark: '#db4437',
+      },
+    ]
 
-      return {
-        isPasswordVisible,
-        email,
-        password,
-        socialLink,
+    return {
+      isPasswordVisible,
+      email,
+      password,
+      socialLink,
 
-        icons: {
-          mdiEyeOutline,
-          mdiEyeOffOutline,
-        },
+      icons: {
+        mdiEyeOutline,
+        mdiEyeOffOutline,
+      },
+    }
+  },
+  methods: {
+    async login() {
+      const res = await axios.post('https://notas-unicaes-api.herokuapp.com/api/login', {
+        email: this.email,
+        password: this.password,
+      })
+      console.log(res.data)
+      if (res.status) {
+        window.location.href = 'http://localhost:8080/dashboard'
       }
     },
-  }
+  },
+}
 </script>
 
 <style lang="scss">
-  @import '~@/plugins/vuetify/default-preset/preset/pages/auth.scss';
+@import '~@/plugins/vuetify/default-preset/preset/pages/auth.scss';
 </style>
---
+++ src/views/pages/Teacher/Teacher.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <p class="text-2xl mb-6">
+      Docentes
+    </p>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="Buscar docente"
+              single-line
+              hide-details
+            ></v-text-field>
+            <v-btn color="primary">
+                Agregar nuevo
+              </v-btn>
+          </v-card-title>
+          <v-data-table
+            :headers="headers"
+            :items="names"
+            :items-per-page="10"
+            :search="search"
+            class="elevation-1"
+          >
+            <template>
+              <v-icon
+                small
+                class="mr-2"
+              >
+                mdi-pencil
+              </v-icon>
+              <v-icon small>
+                mdi-delete
+              </v-icon>
+            </template>
+          </v-data-table>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {},
+
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Apellidos',
+          align: 'start',
+          value: 'lastname',
+        },
+        { text: 'Nombre', value: 'name' },
+        { text: 'e-mail', value: 'email' },
+        { text: 'Telefono', value: 'number' },
+        { text: 'Acciones', value: 'actions', sortable: false },
+      ],
+      names: [
+        {
+          lastname: 'Aguilar Melgar',
+          name: 'Julio Isrrael',
+          email: '2019AG000@sj.com',
+          number: '7855-5598',
+        },
+        {
+          lastname: 'Carranza rivas',
+          name: 'Mario Ernesto',
+          email: '2018CR001@sj.com',
+          number: '5885-5566',
+        },
+        {
+          lastname: 'Palacios Ayala',
+          name: 'Diego Ernesto',
+          email: '2019PA002@sj.com',
+          number: '5845-6969',
+        },
+      ],
+    }
+
+    // Barra de progreso
+  },
+}
+</script>
